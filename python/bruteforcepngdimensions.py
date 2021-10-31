0187f81a40610992a646553eff974132e8406d70
Sean Shin <30362241+AmIAHuman@users.noreply.github.com>
2021-10-31 16:29:33 -0700

    Create bruteforcepngdimensions.py

---
+++ python/bruteforcepngdimensions.py
@@ -0,0 +1,19 @@
+import binascii
+import struct
+
+crc32key = 0x0DB3F6C0 # Change this value
+for i in range(0, 65535):
+  height = struct.pack('>i', i)
+  for x in range(0, 65535):
+    width = struct.pack('>i', x)
+    print("Height: " + height + " Width: " + width)
+    data = '\x49\x48\x44\x52' + width + height + '\x08\x06\x00\x00\x00'
+
+    crc32result = binascii.crc32(data) & 0xffffffff
+
+    if crc32result == crc32key:
+      print "Got Height: "
+      print ''.join(map(lambda c: "%02X" % ord(c), height))
+      print "Got Width: "
+      print ''.join(map(lambda c: "%02X" % ord(c), width))
+      exit(0)
